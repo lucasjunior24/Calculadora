1095ba5a45438bb5895a578b0baa055d5262e5fa
lucasjunior24 <53240060+lucasjunior24@users.noreply.github.com>
2020-07-28 21:16:16 -0500

    Adicionando logica da calculadora - parte I

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,21 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+// estado inicial da calculadora
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    // dizer se estou manipulando o valor de indice 0 ou de indice 1
+    current: 0 // traducao: atual
+}
+
 export default class Calculator extends Component {
+
+    // Usando o operador stred para crir um clone do objeto initialState e atribuir ele a state
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
@@ -13,20 +27,45 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('Limpar')
+        this.setState({ ...initialState })
     }
     setOperation(operation) {
         console.log(operation)
     }
     addDigit(n) {
-        console.log(n)
+        // regra para evitar ter dois pontos na calculadora  
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+        // valor corrente (atual)
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        // se foi digitado um valor diferente de ponto 
+        if (n !== '.') {
+
+            // pege o indice do valor alterado
+            const i = this.state.current
+            // convertendo o indice para float
+            const newValue = parseFloat(displayValue)
+            // armazene esse valor em values
+            const values = [...this.state.values]
+            // adicionando novo valor no array com indice escolhido
+            values[i] = newValue
+            // e adicone o novo array dentro de state
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
 
         return (
             <div className='calculator'>
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label='AC' click={this.clearMemory} triple />
                 <Button label='/' click={this.setOperation} operation />
                 <Button label='7' click={this.addDigit} />
